51c72fdcc7d865834d131223b88e15fcb2948fea
Simon507 <Evgeny.klysak@gmail.com>
2023-03-20 17:19:12 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, is MY first project</Text>
+      <Text>Hello, is MY first project = TEST GIT</Text>
       <StatusBar style="auto" />
     </View>
   );
